d249bf8af8ffe283c31d2c919796c041a3eb2fe9
Christopher McComas <cmccomas@marshall.edu>
2020-10-08 17:10:09 -0400

    wip

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,7 +9,7 @@
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
 
-        <meta name="apple-mobile-web-app-status-bar-style" content="#f7fafc">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
         <link href="/assets/iphonex_splash.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
         <link href="/assets/iphonexr_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
