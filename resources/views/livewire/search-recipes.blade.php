6bc7125dd4aec4c8a795cf11817db468a53eb140
Christopher McComas <cmccomas@marshall.edu>
2020-10-10 13:03:40 -0400

    remove favorite from search list

--- resources/views/livewire/search-recipes.blade.php
+++ resources/views/livewire/search-recipes.blade.php
@@ -6,7 +6,7 @@
 
         <ul class="border border-gray-300 rounded-md divide-y divide-gray-300 mt-4">
             @forelse ($recipes as $recipe)
-                <li><a class="flex py-4 px-4 hover:bg-gray-100" href="/recipe/{{ $recipe->slug }}" class="text-green-500 hover:text-green-800 hover:underline"><span class="{{ $recipe->favorite ? 'font-semibold' : '' }}">{{ $recipe->title }}</span> @if($recipe->favorite) <span class="ml-2 uppercase rounded bg-green-400 text-white text-xs font-semibold px-3 py-1">Favorite</span> @endif</a></li>
+                <li><a class="flex py-4 px-4 hover:bg-gray-100" href="/recipe/{{ $recipe->slug }}" class="text-green-500 hover:text-green-800 hover:underline"><span class="{{ $recipe->favorite ? 'font-semibold' : '' }}">{{ $recipe->title }}</span></a></li>
             @empty
                 <li class="flex py-4 px-4">No recipes found matching that search term</li>
             @endforelse
