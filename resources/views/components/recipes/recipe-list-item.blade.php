211052e6a126f38f770bc37fedd69a41b1c86f0c
Christopher McComas <mccomas.chris@gmail.com>
2022-01-07 09:20:34 -0500

    fix spacing on tags show heading

--- resources/views/components/recipes/recipe-list-item.blade.php
+++ resources/views/components/recipes/recipe-list-item.blade.php
@@ -1,5 +1,5 @@
 <div class="snap-start shrink-0 lg:w-1/3 lg:px-4">
-    <div class="h-full border border-stone-200 rounded flex flex-col w-72 lg:w-auto">
+    <div class="h-full border border-stone-200 rounded flex flex-col w-72 lg:w-auto bg-stone-50">
         @if ($recipe->image_url)
             <div class="h-48 bg-cover bg-center" style="background: url({{ $recipe->image_url }})"></div>
         @else
--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -6,7 +6,7 @@
 @endsection
 
 @section('content')
-    <div class="lg:sticky lg:top-0 bg-stone-100 lg:pt-4 pb-8 lg:border-b border-stone-200">
+    <div class="lg:sticky lg:top-0 bg-stone-100 lg:pt-4 pb-8 lg:border-b border-stone-200 mb-12">
         <div class="flex flex-wrap items-center justify-between">
             <div class="w-full lg:w-1/2 text-center lg:text-left">
                 <h1 class="leading-none mb-0 flex-1">{{ $tag->name }} Recipes</h1>
@@ -14,7 +14,7 @@
             <div class="w-full lg:w-1/2 mt-6 lg:mt-0">
                 <div class="flex flex-col items-center lg:items-end">
                     <label for="nav" class="text-sm font-semibold text-stone-600 uppercase">Other Categories</label>
-                    <select onchange="window.location.href = this.value;" id="nav" name="nav" class="flex-1 mt-2 block w-full pl-3 pr-10 py-2 text-base border-stone-200 focus:outline-none focus:ring-green-700 focus:border-green-700 sm:text-sm rounded-md">
+                    <select onchange="window.location.href = this.value;" id="nav" name="nav" class="flex-1 mt-2 block w-auto pl-3 pr-10 py-2 text-base border-stone-200 focus:outline-none focus:ring-green-700 focus:border-green-700 sm:text-sm rounded-md">
                         @foreach($tags as $tag_item)
                             <option value="{{ route('tag.show', $tag_item->slug) }}" {{ $tag->id === $tag_item->id ? 'selected' : '' }}>{{ $tag_item->name }}</option>
                         @endforeach
